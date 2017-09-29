3957c95a74f944633d230dfc5f0c0a0b5daff4ea
USP <USP@DESKTOP-TNO3FK2>
2017-09-28 22:05:24 -0500

    segundo avance de clase de la semana 7

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ class Tarea{
         else
             return $n2;
     }
+    function menorDosNumeros(){
+        return 0;
+    }
 }
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
